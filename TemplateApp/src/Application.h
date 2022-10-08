56d1a51c6f41411f9a06b59ef0d8da01e2230d32
Erik <92889691+erikrl2@users.noreply.github.com>
2022-10-08 16:35:52 +0200

    Simplify delta time calculation

--- TemplateApp/src/Application.h
+++ TemplateApp/src/Application.h
@@ -16,20 +16,15 @@ public:
 		sf::RenderWindow window;
 		App app(&window, std::forward<Args>(args)...);
 
+		sf::Clock clock;
+
 		while (window.isOpen())
 		{
 			sf::Event event;
 			while (window.pollEvent(event))
 				app.OnEvent(event);
 
-			static sf::Clock clock;
-			static sf::Time lastTime = sf::Time::Zero;
-
-			sf::Time time = clock.getElapsedTime();
-			sf::Time deltaTime = time - lastTime;
-			lastTime = time;
-
-			app.Update(deltaTime);
+			app.Update(clock.restart());
 		}
 	}
 };
